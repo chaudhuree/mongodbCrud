e41fa2fd2066aaa74ec3d53d865f831fede4636d
chaudhuree <chaudhuree00@gmail.com>
2022-11-18 18:03:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const { showAll, deleteDatabase, insertSingle, insertMultiple, findSingleData, findMultipleData, deleteSingleData, deleteMultipleData,updateSingleDoc,updateMultipleDocs,replaceDoc }=require("./mdbFunctions")
+
+// Example:
+// showAll("database name","collection name")
